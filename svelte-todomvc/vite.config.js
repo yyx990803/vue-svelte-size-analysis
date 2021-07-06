bed2f3a820914a6674162356967cf5776c7a5cc9
Evan You <yyx990803@gmail.com>
2021-07-06 09:41:51 -0400

    more accurate ssr data

--- svelte-todomvc/vite.config.js
+++ svelte-todomvc/vite.config.js
@@ -5,8 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte({
     compilerOptions: {
-      // doesn't seem to work?
-      // hydratable: true
+      hydratable: false
     }
   })]
 })
